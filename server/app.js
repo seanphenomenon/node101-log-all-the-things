957f90ffe69c86a3bb7a00a8177eb86f0647eeee
Sean Harrell <seanvharrell@gmail.com>
2020-03-17 05:04:45 +0000

    converted csv to json and displayed on webpage

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
-const loggerFile = './server/log.csv';
 const chalk = require('chalk');
+const loggerFile = './server/log.csv';
+const csvtojson = require('csvtojson');
 
 
 const app = express();
@@ -10,93 +11,95 @@ const app = express();
 
 app.use(morgan('dev'));
 
-var log = [];
-
-var logger = {
-    Agent:'',
-    Time:'',
-    Method:'',
-    Resource:'',
-    Version:'',
-    Status:''
-  }
 
 app.use((req, res, next) => {
-// write your logging code here
+  // write your logging code here
 
-// found each specific piece of log data to display in console below
-var agent = req.headers['user-agent']
-console.log("agent -", (chalk.yellowBright(agent)));
+  // found each specific piece of log data to display in console below:
+  var agent = req.headers['user-agent'].replace(',');
+  console.log("agent -", (chalk.yellowBright(agent)));
 
-var time = new Date().toISOString();
-console.log("time -",(chalk.greenBright(time)));
+  var time = new Date().toISOString();
+  console.log("time -", (chalk.greenBright(time)));
 
-var method = req.method;
-console.log("method -", (chalk.cyanBright(method)));
+  var method = req.method;
+  console.log("method -", (chalk.cyanBright(method)));
 
-var resource = req.originalUrl;
-console.log("resource -",(chalk.blueBright(resource)));
+  var resource = req.originalUrl;
+  console.log("resource -", (chalk.blueBright(resource)));
+
+  var version = req.protocol;
+  console.log("version -", (chalk.magentaBright(version)));
+
+  var status = res.statusCode;
+  console.log("status -", (chalk.whiteBright(status)));
+
+  // mimicking in console how data needs to display in log file :
+  var logData = (`${agent},${time},${method},${resource},${version},${status}`);
+  console.log(logData);
 
 
-var version = req.protocol;
-console.log("version -",(chalk.magentaBright(version)));
+  // appends data to log file:
+  fs.appendFile(loggerFile, agent + ',', function (err) {
+    if (err) throw err;
+    console.log('Agent Saved!');
+  });
 
-var status = res.statusCode;
-console.log("status -", (chalk.whiteBright(status)));
+  fs.appendFile(loggerFile, time + ',', function (err) {
+    if (err) throw err;
+    console.log('Time Saved!');
+  });
 
-// mimicking how data needs to display below
-var logData =(`${agent},${time},${method},${resource},${version},${status}`);
-console.log(logData);
+  fs.appendFile(loggerFile, method + ',', function (err) {
+    if (err) throw err;
+    console.log('Method Saved!');
+  });
+
+  fs.appendFile(loggerFile, resource + ',', function (err) {
+    if (err) throw err;
+    console.log('Resource Saved!');
+  });
+
+  fs.appendFile(loggerFile, version + ',', function (err) {
+    if (err) throw err;
+    console.log('Version Saved!');
+  });
+
+  fs.appendFile(loggerFile, status, function (err) {
+    if (err) throw err;
+    console.log('Status Saved!');
+  });
 
 
-// appends data to log file
-fs.appendFile(loggerFile, agent +', ', function (err) {
-  if (err) throw err;
-    console.log('Saved!');
+  next();
 });
 
-fs.appendFile(loggerFile, time +', ', function (err) {
-if (err) throw err;
-console.log('Saved!');
-  });
 
-fs.appendFile(loggerFile, method +', ', function (err) {
-if (err) throw err;
-  console.log('Saved!');
-  });
+app.get('/', (req, res) => {
+  // write your code to respond "ok" here
 
-fs.appendFile(loggerFile, resource +', ', function (err) {
-if (err) throw err;
-  console.log('Saved!');
-  });
-
-fs.appendFile(loggerFile, version +', ', function (err) {
-if (err) throw err;
-  console.log('Saved!');
-  });
-
-fs.appendFile(loggerFile, status, function (err) {
-if (err) throw err;
-  console.log('Saved!');
-  });
-
-
-next();
-});
-
-
-app.get('/', (req, res) =>{
-// write your code to respond "ok" here
-res.status(200).send('OK')
+  res.status(200).send('OK')
 });
 
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
-res.json([loggerFile])
+  // write your code to return a json object containing the log data here
 
+  // utf8 converted raw buffer data to string of text
+  fs.readFile('./server/log.csv', 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    // console.log(data);
 
-res.status(200).send("logs");
-});
+    // converts csv to json:
+  csvtojson().fromFile(loggerFile).then(data => {
+    console.log(data);
+    res.status(200).send(data);
+    res.end();
+    });
+
+  });
+})
 
 module.exports = app;
